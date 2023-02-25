863c2aa30d87b84eb72c0dc1d779289fbd7dfc09
anneozolin <anelisa.ozolin@gmail.com>
2023-02-24 22:17:56 -0300

    W01 programming assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("What is your first name?");
+        string fistName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"Your name is {lastName}, {fistName} {lastName}.");
     }
+
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+
+        string gradeLetter = "";
+
+        if (gradePercentage >= 90) 
+            {
+                gradeLetter = "A";
+            }
+        else if (gradePercentage >= 80)
+            {
+                gradeLetter = "B";
+            }
+        else if (gradePercentage >= 70)
+            {
+                gradeLetter = "C";
+            }
+        else if (gradePercentage >= 60)
+            {
+                gradeLetter = "D";
+            }
+        else
+            {
+                gradeLetter = "F";
+            }
+        
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
